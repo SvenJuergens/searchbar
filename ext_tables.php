304ca3b07e744b96649f70269a8e9239541d8828
Sven Jürgens <sj@nordsonne.de>
2015-04-09 12:56:43 +0200

    moved flexform_ds.xml to Configuration Folder

--- ext_tables.php
+++ ext_tables.php
@@ -14,7 +14,7 @@ if (TYPO3_MODE == 'BE')    {
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key,pages';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1'] = 'pi_flexform';
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY .'_pi1', 'FILE:EXT:searchbar/flexform_ds.xml');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY .'_pi1', 'FILE:EXT:searchbar/Configuration/FlexForms/flexform_ds.xml');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array(
 	'LLL:EXT:searchbar/locallang_db.xml:tt_content.list_type_pi1',
