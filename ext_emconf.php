de5f6b127004d18dbbe32730c7b6711208510349
Sven Jürgens <sj@nordsonne.de>
2015-04-12 20:07:49 +0200

    Version 2.0.0 und TYPO3 Version max 7.1.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'title' => 'Universal Search Bar',
 	'description' => 'One Search Bar to rule them all... . A Search Bar "wrapper", to access all existing search engines. More on search.blue-side.de',
 	'category' => 'fe',
-	'version' => '2.0.0-dev',
+	'version' => '2.0.0',
 	'state' => 'stable',
 	'uploadfolder' => 1,
 	'createDirs' => '',
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array (
 	array (
 		'depends' => 
 		array (
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-7.1.99',
 		),
 		'conflicts' => 
 		array (
